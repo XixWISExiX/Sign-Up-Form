91d91af03e7f1308af8e1b120f73b2f72e42f96a
XixWISExiX <addalooptoit@gmail.com>
2023-05-26 23:03:43 -0500

    Added Script for continuous password varification

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 function application() {
   let btn = document.getElementsByClassName("account-btn");
+  document.addEventListener("input", function () {
+    if (formApp.passwordFlag) {
+      btn[0].addEventListener("click", function (event) {
+        submitPressed();
+      });
+      submitPressed();
+    }
+  });
   btn[0].addEventListener("click", function (event) {
+    formApp.passwordFlag = true;
     submitPressed();
   });
 }
+
+let formApp = (function () {
+  let passwordFlag = false;
+  return { passwordFlag };
+})();
 
 function submitPressed() {
   passwordEnteredIncorrectly();
@@ -12,23 +26,27 @@ function submitPressed() {
 
 function passwordEnteredIncorrectly() {
   let password = document.getElementById("password");
+  let msg = document.getElementsByClassName("password-text");
   if (password.value === "") return;
   if (!password.validity.valid) {
     event.preventDefault();
-    let msg = document.getElementsByClassName("password-text");
     msg[0].innerText =
       "* Password must be at least 10 characters long, and include at least one number, one letter, and one special symbol.";
+  } else {
+    msg[0].innerText = "";
   }
 }
 
 function equalPasswordFields() {
   let password = document.getElementById("password");
   let confirmPassword = document.getElementById("confirm_password");
+  let msg = document.getElementsByClassName("confirm_password-text");
   if (confirmPassword.value === "") return;
   if (password.value !== confirmPassword.value) {
     event.preventDefault(); // prevent form submission
-    let msg = document.getElementsByClassName("confirm_password-text");
     msg[0].innerText = "* Passwords do not match.";
+  } else {
+    msg[0].innerText = "";
   }
 }
 
